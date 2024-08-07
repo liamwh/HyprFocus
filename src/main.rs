a516ffc5d27b793feb107dd434098b1c243adf21
Liam Woodleigh-Hardinge <liam.woodleigh@gmail.com>
2024-08-07 18:33:42 +0200

    feat(wip): Get command & json printing
    feat: Pretty printing

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use anyhow::Result;
 
 mod app_cmds;
 mod focus;
+mod get;
 mod io;
 mod observability;
 
@@ -10,14 +11,32 @@ use focus::focus_or_launch_app;
 #[tokio::main]
 async fn main() -> Result<()> {
     let _non_blocking_writer_guard = observability::setup_tracing()?;
-    let arg_matches = app_cmds::app_cmd().get_matches();
-    let client_name = arg_matches
-        .get_one::<String>("client")
-        .expect("expected to valid 'client name' arg");
+    let matches = app_cmds::app_cmd().get_matches();
+    if let Some(get_matches) = matches.subcommand_matches("get") {
+        if let Some(class_matches) = get_matches.subcommand_matches("class") {
+            if let Some(class_name) = class_matches.get_one::<String>("class") {
+                let using_json_output = class_matches.get_flag("json");
+                let clients_with_launcher_commands = get::get_launcher_cmd(class_name)?;
+                if using_json_output {
+                    let json = serde_json::to_string_pretty(&clients_with_launcher_commands)?;
+                    println!("{json}");
+                    return Ok(());
+                }
+                for client_with_launcher_command in clients_with_launcher_commands {
+                    println!("{client_with_launcher_command}");
+                }
+            }
+        }
+    } else {
+        let class_name = matches
+            .get_one::<String>("class")
+            .expect("expected a valid 'class' arg");
 
-    let launcher_command = arg_matches
-        .get_one::<String>("launcher")
-        .expect("expected to valid 'launcher' arg");
+        let launcher_command = matches
+            .get_one::<String>("launcher")
+            .expect("expected a valid 'launcher' arg");
 
-    focus_or_launch_app(client_name, launcher_command)
+        return focus_or_launch_app(class_name, launcher_command);
+    }
+    Ok(())
 }
